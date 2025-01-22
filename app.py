45d153e570627783b238c569845b676a090175a1
Ruslan Aukhadyev <78228760+Excavator0@users.noreply.github.com>
2025-01-22 23:15:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 from lab13_1.lab13_1 import lab_13_1
 from lab13_2.lab13_2 import lab_13_2
+from poll_temps import poll_temps
 
 app = Flask(__name__)
 app.register_blueprint(lab_13_1)
 app.register_blueprint(lab_13_2)
+app.register_blueprint(poll_temps)
 with open('modbusRESTAPI/config.json') as f:
     d = json.load(f)
 

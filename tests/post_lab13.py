f647041bfc685ac5bcac899d50265c4ec5a115f6
Ruslan Aukhadyev <78228760+Excavator0@users.noreply.github.com>
2025-02-13 17:11:25 +0300

    тест новой ручки лаб 13

---
+++ tests/post_lab13.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def test_post_set_valve():
+    value = "release" # значение для передачи в запросе
+    url = "http://127.0.0.1:3000/lab14/trm210/set_voltage"
+
+    # выполнение POST-запроса
+    response = requests.post(f"{url}?value={value}")
+
+    # вывод результата запроса
+    print("Status Code:", response.status_code)
+    print("Response Body:", response.text)
+
+
+test_post_set_valve()
